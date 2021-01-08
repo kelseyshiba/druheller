f43261570d39a0738c5213c900c41dad5937001b
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-08 14:46:16 -0700

    added about and lessons pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from '../components/layout.js';
+import { SEO } from '../components/seo.js';
+
+const About = () => {
+    <Layout>
+        <SEO title='dru heller bio page'/>
+        <div>
+            <h1>About Dru Heller</h1>
+            <p>This is my bio yo.</p>
+        </div>
+    </Layout>
+}
+export default About;
---
+++ src/pages/lessons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from '../components/layout.js';
+import { SEO } from '../components/seo.js';
+
+const Lessons = () => {
+    <Layout>
+        <SEO title='dru heller lessons page'/>
+        <div>
+            <h1>Lessons with Dru</h1>
+            <p>Here is information on lessons with Dru.</p>
+        </div>
+    </Layout>
+}
+
+export default Lessons;
