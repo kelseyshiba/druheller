151e0acbf680a87c2f6cee37ddb5255ce7acd754
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-15 11:35:37 -0700

    create live page

---
+++ src/pages/live.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+import SEO from '../components/seo.js';
+
+const Live = () => (
+    <Layout>
+        <SEO title='Dru Heller Live page'/>
+        <div>
+            <h1>Live!</h1>
+            <p>Gig Calendar Information</p>
+        </div>
+    </Layout>
+)
+
+export default Live;
