ef4c91c7c78efd99a06a173c852610acacb64de7
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-15 11:35:14 -0700

    created contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+import SEO from '../components/seo.js';
+
+const Contact = () => (
+    <Layout>
+        <SEO title='Dru Heller Contact page'/>
+        <div>
+            <h1>Contact Dru</h1>
+            <p>druheller@gmail.com</p>
+        </div>
+    </Layout>
+)
+
+export default Contact;
