90e51e069586361a0ce9c6db392749805b647032
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-15 11:35:55 -0700

    added banner to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Banner from "../components/banner"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Banner/>
     <h1>Welcome to the Official Dru Heller Music Website!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
