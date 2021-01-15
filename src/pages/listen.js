66c0ee24d1cec70b0179e082c03663d7eac8d70a
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-15 11:35:31 -0700

    created listen page

---
+++ src/pages/listen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+import SEO from '../components/seo.js';
+
+const Listen = () => (
+    <Layout>
+        <SEO title='Dru Heller Listen page'/>
+        <div>
+            <h1>Listen</h1>
+            <p>link or embeded files</p>
+        </div>
+    </Layout>
+)
+
+export default Listen;
