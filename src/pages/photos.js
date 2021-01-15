dcc261d6e0d78a54e83bb1fd867b4e51fad6e313
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-15 11:35:44 -0700

    create photo page

---
+++ src/pages/photos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+import SEO from '../components/seo.js';
+
+const Photos = () => (
+    <Layout>
+        <SEO title='Dru Heller photos page'/>
+        <div>
+            <h1>Photos of Dru</h1>
+            <p>Professional Photos of Dru here</p>
+        </div>
+    </Layout>
+)
+
+export default Photos;
