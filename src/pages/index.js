847923a069d64b3049d21a30018ca6d1cb3ccae3
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-20 11:51:03 -0700

    removed image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Banner from "../components/banner"
 
@@ -10,9 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Banner/>
-    <h1>Welcome to the Official Dru Heller Music Website!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
