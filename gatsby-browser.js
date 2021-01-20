b23f7acae3507afde6b03d77a8e64898ce144c32
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-19 18:57:51 -0700

    added bootstrap for later

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@
  */
 
 // You can delete this file if you're not using it
-import './bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap';
+import './src/styles/global.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'jquery/dist/jquery.min.js'
 // import 'bootstrap/dist/js/bootstrap.min.js'
