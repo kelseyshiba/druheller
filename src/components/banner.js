1b584ffd397dc600e7ebf74290d9ee869ae3713b
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-19 18:58:27 -0700

    removed bootstrap to practice css

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,41 +1,22 @@
 import React from 'react';
-import { Col, Row, Card } from 'react-bootstrap';
 import heller1 from '../images/heller1.jpg';
 import heller2 from '../images/heller2.jpg';
 import heller3 from '../images/heller3.jpg';
 import hellersign from '../images/hellersign.png';
 
 const Banner = () => (
-    <section id='banner'>
-        <div className='inner'>
-            <header className='container'>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '10rem' }}>
-                            <Card.Img variant='top' src={heller1} />
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '10rem' }}>
-                            <Card.Img variant='top' src={heller2} />
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '10rem' }}>
-                            <Card.Img variant='top' src={heller3} />
-                        </Card>
-                    </Col>
-                    <Col>
-                        <img src={hellersign}/>
-                    </Col>
-                </Row>
-                <Row></Row>
-            </header>
-            <div className='content'>
-                <p>Hello Hello</p>
-            </div>
+    <div id='banner'>
+        <div id='hellerphotos'>
+            <img src={heller1} alt='heller 1'/>
+            <img src={heller2} alt='heller 2'/>
+            <img src={heller3} alt='heller 3'/>
         </div>
-    </section>
+        <h1 id='bannerName'>DRU HELLER</h1>
+        <h3 id='bannerText'>Drummer • Artist • Educator • Clinician</h3>
+        <div id='signature'>
+            <img src={hellersign} alt='heller signature'/>
+        </div>
+    </div>
 )
 
 export default Banner;
