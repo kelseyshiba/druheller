c95a965b75f0070361bb66ed207812f7434c3f2b
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-21 16:44:08 -0700

    clean up home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,11 +8,9 @@ import Banner from "../components/banner"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Banner/>
+    <Banner />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
