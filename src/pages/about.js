161d38cc65c60e5410073bff863f4ffa855e9872
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-21 16:44:57 -0700

    added image and bio to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import hellerbw from '../images/hellerbw.jpg'
 
 const About = () => (
     <Layout>
         <SEO title='dru heller bio page'/>
-        <div>
-            <h1>About Dru Heller</h1>
-            <p>This is my bio yo.</p>
+        <h1>Biography</h1>        
+        <div className='row'>
+            <div className='column'>
+                <img src={hellerbw} alt='heller biophoto' id='biophoto'/>
+            </div>
+
+            <div className='column' style={{fontFamily: 'Open Sans', padding: '10px'}}>
+                <p><strong>DRU HELLER</strong> is an   expressive, authentic, and joyful drummer,composer, and bandleader.His passionate playing and love for music has opened opportunities for him to perform and teach all over the country.</p>
+                <p>Known for his dynamic and interactive playing, his career spans over two decades. He has toured and recorded with some of the greatest musicians in jazz, including Don Byron, John Ellis, Ron Miles, Art Lande and  Greg Osby. He was a featured performer on Don Byron’s original score for the American  Masters (PBS) documentary of Lorraine Hansberry: <em> Sighted Eyes Feeling Heart</em>. His  own quartets havereceived accolades and   garnered attention from jazz enthusiasts   worldwide, and he is a first-calldrummer for the bandstands in the greater Denver area.</p>
+                <p>Dru is an engaged and empathetic    teacher. He made his home in Denver,   Colorado after he received a Master of Music degree from the University of     Colorado. He has served as an  artist-in-residence at multiple schools and festivals, and he is a music faculty member at Metropolitan State University of Denver and the University of Colorado- Denver. He is able to reach students of all ages and ability levels and maintains his own private studio.</p>
+                <p>On and off the bandstand, Dru is an active listener. His vibrant spirit can be felt and heard through the flick of his sticks or the wit and charm of his conversations. For more information, go to druhellermusic.com.
+                </p>
+            </div>
         </div>
+        <div></div>
     </Layout>
 )
 export default About;
