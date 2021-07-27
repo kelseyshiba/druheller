42fd893f039f737178cc2839ad590fca3c74f4cd
Elijah Samuels <elijahsamuels@gmail.com>
2021-07-27 00:36:08 -0600

    added embeded youtube videos and youtubeembed component

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import Layout from '../components/layout.js';
-import SEO from '../components/seo.js';
+import React from "react"
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
+import YoutubeEmbed from "../components/youtubeEmbed"
 
 const Listen = () => (
-    <Layout>
-        <SEO title='Dru Heller Listen page'/>
-        <div>
-            <h1>Listen</h1>
-            <p>link or embeded files</p>
-        </div>
-    </Layout>
+  <Layout>
+    <SEO title="Dru Heller Listen page" />
+    <div align="center">
+      <h1>Listen</h1>
+
+      <div className="App">
+        <YoutubeEmbed embedId="Z2jzvpaYNzI" />
+        <YoutubeEmbed embedId="aKyPGGZHwgc" />
+        <YoutubeEmbed embedId="6EcqXCND_6w" />
+        <YoutubeEmbed embedId="hfmPjpAi0lI" />
+        <YoutubeEmbed embedId="6eRG6_C3K-A" />
+
+      </div>
+    </div>
+  </Layout>
 )
 
-export default Listen;
+export default Listen
